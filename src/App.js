bcff9e461b26506254455be10be1da72b58f1cb4
Luiz Mendes <luiz.mendes@in8.com.br>
2021-10-19 21:28:28 -0300

    feat: message in app iinstalled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 
 function App() {
+  window.addEventListener('appinstalled', (evt) => {    alert("Bem vindo ao Printer3D");});
   return (
     <div className="App">
       <Home/>
